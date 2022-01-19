d1e3cab660a60edbf1f41da40cea5b41482dba5a
Rodrigo Brito <rodrigo.lab@yahoo.com>
2022-01-19 15:54:51 -0300

    Layout feito até agora

---
+++ src/components/CardProduto.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import xbox from "./img/xbox.jpeg"
+
+const CardProduto = () => {
+    return(
+
+        <div className='Card'>
+            {/* <img src={xbox} /> */}
+            <p> Produto tal </p> 
+            
+        </div>
+    )
+}
+
+export default CardProduto
